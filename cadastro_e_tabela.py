ca40e2ba20f708756b91ac65466ba2fc94d49385
Francisco Abraão <75095166+Fagoneto@users.noreply.github.com>
2025-02-06 11:26:39 -0300

    Add codes and data

---
+++ cadastro_e_tabela.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+from dependencies_final import add_registro, consulta_geral, consulta
+
+
+db_query = consulta_geral()
+
+registros = {'usernames': {}}
+for data in db_query:
+    registros['usernames'][data[1]] = {'name' : data[0], 'password' : data[2]}
+
+COOKIE_EXPIRY_DAYS = 30
+authenticator = stauth.Authenticate(
+    registros,
+    'random_cookie_name',
+    'random_signature_key',
+    COOKIE_EXPIRY_DAYS,
+
+)
+
+def usuario_form():
+    with st.form(key="test", clear_on_submit=True):
+        nome = st.text_input("Nome", key="nome")
+        username = st.text_input("Usuário", key="user")
+        password = st.text_input("Password", key="pswrd", type="password")
+        confirm_password = st.text_input("Confirm Password", key="confirm_pswrd", type="password")
+        submit = st.form_submit_button(
+            "Salvar", on_click=confirmation_msg,
+        )
+
+def confirmation_msg():
+    hashed_password = stauth.Hasher([st.session_state.pswrd]).generate()
+    if st.session_state.pswrd != st.session_state.confirm_pswrd:
+        st.warning('Senhas não conferem')
+        #sleep(3)
+    elif consulta(st.session_state.user):
+        st.warning('Nome de usuário já existe.')
+        #sleep(3)
+    else:
+        add_registro(st.session_state.nome,st.session_state.user, hashed_password[0])
+        st.success('Registro efetuado!')
+        #sleep(3)
+
+
+def main():
+    st.title("Cadastro de Usuários")
+    usuario_form()
+
+    # Exibir a tabela de cadastrados
+    st.title("Tabela de Cadastrados")
+    registros = consulta_geral()
+    if registros:
+        st.table(registros)
+    else:
+        st.info("Nenhum cadastro encontrado.")
+
+
+
+if __name__ == "__main__":
+    main()
